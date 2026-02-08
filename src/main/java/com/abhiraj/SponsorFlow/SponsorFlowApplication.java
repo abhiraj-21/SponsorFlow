3ddcbef1ad4083a4e6ce447bb3288fbf775ac2d2
abhirajsingh <abhirajsinghchouhan4@gmail.com>
2026-02-08 12:41:50 +0530

    created repository

---
+++ src/main/java/com/abhiraj/SponsorFlow/SponsorFlowApplication.java
@@ -0,0 +1,13 @@
+package com.abhiraj.SponsorFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsorFlowApplication.class, args);
+	}
+
+}
